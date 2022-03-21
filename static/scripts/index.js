030dfa261a10226bf8fcf6b4b2f0f83afe39d47d
Kaihua Liu <lkaihua@gmail.com>
2022-03-21 13:00:41 +0000

    Fix trailing item width

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,25 +1,34 @@
-/* Find the trailing item in each row, add a new class.
- * The trailing item in Breadcrumb will take up the spaces until the row end
- * So it won't leave an ugly blank space.
+/* Find the trailing item in each row, add a new class and update max width.
  *
- * This solution also benefits from the stable widths of items, otherwise the everchanging widths 
- * distracts the user when clicking folder links.
+ * Force the trailing pill in each row to extend to the end so it won't leave an ugly blank
+ * and all items have stable width between page jumps.
  */
-function addTopBreadcrumbTralingItems() {
+function initTopBreadcrumbTralingItems() {
     const elements = document.querySelectorAll("#TopBreadcrumb li")
     let largestDistance = -1
+    let lastMaxWidth = 0;
     let lastElement;
-    
+    const zero = elements[0] && elements[0].offsetLeft
+    const gap = 5
+
     Array.from(elements).reduce(
         (acc, current) => {
             if (current.offsetLeft <= largestDistance) {
-                lastElement && acc.push(lastElement)
+                lastElement && acc.push([lastElement, lastMaxWidth])
             }
+            
+            if (lastElement) {
+                lastMaxWidth = lastElement.offsetLeft + lastElement.offsetWidth - zero + gap
+                console.debug(current, lastMaxWidth)
+            }
+            
+            
             largestDistance = current.offsetLeft
             lastElement = current
             return acc
         }, []
     ).forEach(e => {
-        e.classList.add("row-trailing-item")
+        e[0].classList.add("row-trailing-item")
+        e[0].style.maxWidth = `calc(100% - ${e[1]}px)`
     })
 }
